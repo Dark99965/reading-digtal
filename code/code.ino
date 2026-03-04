77555f137eedbdf801d4ef998281ef017fc58cf4
Dark999565 <noah.retter@icloud.com>
2026-03-03 20:36:47 -0400

    when u press the btn it will say clickde

--- code/code.ino
+++ code/code.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  pinMode(8, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if (digitalRead(8) == LOW) {
+    Serial.println("clicked");
+    delay(500);
+  }
 }
