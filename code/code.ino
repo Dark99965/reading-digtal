5e7f5704d51c8ffdd3242469d27ab73ede3620ef
Dark99965 <noah.retter@icloud.com>
2026-03-05 16:58:32 -0400

    added led so when u press the btn it lights up

--- code/code.ino
+++ code/code.ino
@@ -2,12 +2,14 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(8, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(8) == LOW) {
-    Serial.println("clicked");
-    delay(500);
+    digitalWrite(9, HIGH);
+  } else {
+    digitalWrite(9, LOW);
   }
 }
